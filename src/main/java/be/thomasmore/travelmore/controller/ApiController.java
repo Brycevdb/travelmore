ad11ddfda8af7587e2a629fae8aff0526065c141
greif.matthias <greif.matthias@outlook.com>
2018-10-19 09:23:43 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main/java/be/thomasmore/travelmore/controller/ApiController.java
@@ -0,0 +1,12 @@
+package be.thomasmore.travelmore.controller;
+
+import be.thomasmore.travelmore.domain.Location;
+import be.thomasmore.travelmore.repository.LocationRepository;
+
+import java.util.List;
+
+import static sun.plugin.javascript.navig.JSType.Location;
+
+public class ApiController {
+
+}
---
+++ src/main/java/be/thomasmore/travelmore/controller/HomeController.java
@@ -0,0 +1,39 @@
+package be.thomasmore.travelmore.controller;
+
+import be.thomasmore.travelmore.domain.Location;
+import be.thomasmore.travelmore.repository.LocationRepository;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@Named(value = "homeController")
+@SessionScoped
+public class HomeController implements Serializable {
+
+    private LocationRepository locationRepo;
+
+    private List<Location> locations;
+
+    public HomeController() {
+        this.locationRepo = new LocationRepository();
+    }
+
+    public List<Location> getLocations(String name){
+
+        if(name == null || name == ""){
+            this.locations = this.locationRepo.findAll();
+        }else {
+            this.locations = this.locationRepo.findByName(name);
+        }
+
+        return this.locations;
+    }
+
+    public String index() {
+        return "index";
+    }
+
+
+}
