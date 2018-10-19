e7b77f82ed37072545a0dc4774bf10a2ce8f869d
Brycevdb <vdb.bryce@gmail.com>
2018-10-19 09:11:53 +0200

    Add domain payments

--- src/main/java/be/thomasmore/travelmore/domain/Payments.java
+++ src/main/java/be/thomasmore/travelmore/domain/Payments.java
@@ -1,4 +1,39 @@
 package be.thomasmore.travelmore.domain;
+import javax.persistence.*;
+import java.util.Date;
 
+@Entity
+@Table(name = "payments")
 public class Payments {
+    public static final String FIND_ALL = "payments.findAll";
+    @Id
+    private int id;
+    @Column (name = "userpaymentmethodId")
+    private int userpayementmethodId;
+    @Column (name = "transaction")
+    private Date transaction;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserpayementmethodId() {
+        return userpayementmethodId;
+    }
+
+    public void setUserpayementmethodId(int userpayementmethodId) {
+        this.userpayementmethodId = userpayementmethodId;
+    }
+
+    public Date getTransaction() {
+        return transaction;
+    }
+
+    public void setTransaction(Date transaction) {
+        this.transaction = transaction;
+    }
 }
