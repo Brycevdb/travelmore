382e59bac38c838c2d061b6051d019627aa8f380
Brycevdb <vdb.bryce@gmail.com>
2018-10-19 09:17:03 +0200

    Add domain period

--- src/main/java/be/thomasmore/travelmore/domain/Period.java
+++ src/main/java/be/thomasmore/travelmore/domain/Period.java
@@ -1,4 +1,41 @@
 package be.thomasmore.travelmore.domain;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "period")
+
 public class Period {
+    public static final String FIND_ALL = "period.findAll";
+    @Column (name = "id")
+    private int id;
+    @Column (name = "start")
+    private Date start;
+    @Column (name = "end")
+    private Date end;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date start) {
+        this.start = start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
 }
