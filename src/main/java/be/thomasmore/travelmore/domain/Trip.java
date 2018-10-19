88ab3e497804283391f8da3c1d8b0e4cb19d0826
Brycevdb <vdb.bryce@gmail.com>
2018-10-19 09:42:18 +0200

    Add domain trip

--- src/main/java/be/thomasmore/travelmore/domain/Trip.java
+++ src/main/java/be/thomasmore/travelmore/domain/Trip.java
@@ -1,4 +1,51 @@
 package be.thomasmore.travelmore.domain;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "trip")
+
 public class Trip {
+    public static final String FIND_ALL = "trip.findAll";
+
+    @Column (name = "id")
+    private int trip;
+    @Column (name = "departure")
+    private int departure;
+    @Column (name = "accomodationId")
+    private int accomodationId;
+    @Column (name = "transportId")
+    private int transportId;
+
+    public int getTrip() {
+        return trip;
+    }
+
+    public void setTrip(int trip) {
+        this.trip = trip;
+    }
+
+    public int getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(int departure) {
+        this.departure = departure;
+    }
+
+    public int getAccomodationId() {
+        return accomodationId;
+    }
+
+    public void setAccomodationId(int accomodationId) {
+        this.accomodationId = accomodationId;
+    }
+
+    public int getTransportId() {
+        return transportId;
+    }
+
+    public void setTransportId(int transportId) {
+        this.transportId = transportId;
+    }
 }
