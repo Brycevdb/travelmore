96391aff53b9c9ddfd59338afca3ec53af483faa
Brycevdb <vdb.bryce@gmail.com>
2018-10-19 11:20:38 +0200

    add paymentMethodService

---
+++ src/main/java/be/thomasmore/travelmore/service/PaymentMethodService.java
@@ -0,0 +1,22 @@
+package be.thomasmore.travelmore.service;
+
+import be.thomasmore.travelmore.domain.PaymentMethod;
+import be.thomasmore.travelmore.repository.PaymentMethodRepository;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import java.util.List;
+
+@Stateless
+public class PaymentMethodService {
+    @Inject
+    private PaymentMethodRepository paymentMethodRepository;
+
+    public PaymentMethod findPaymentMethodById(int id){
+        return paymentMethodRepository.findById(id);
+    }
+
+    public List<PaymentMethod> findAllPaymentMethods(){
+        return paymentMethodRepository.findAll();
+    }
+}
