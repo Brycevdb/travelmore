018a1b65f3843d5a87ca96f1b3d7af18aed45dab
Daan <daantjexx3@gmail.com>
2018-10-26 11:14:28 +0200

    index.xhtml

--- src/main/java/be/thomasmore/travelmore/controller/TripOfUserController.java
+++ src/main/java/be/thomasmore/travelmore/controller/TripOfUserController.java
@@ -1,6 +1,14 @@
 package be.thomasmore.travelmore.controller;
 
-public class TripOfUserController {
+import be.thomasmore.travelmore.domain.User;
+import be.thomasmore.travelmore.service.UserService;
+
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+public class TripOfUserController implements Serializable{
 
     public String index(){
 
